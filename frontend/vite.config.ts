91364fd99476f7f448945db0b546298f2bc6e8d6
hjaguen <hjaguen@github.com>
2026-04-17 23:31:31 +0000

    chore: remove api, simplify for deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      }
+    host: '0.0.0.0',
+    hmr: {
+      host: '0.0.0.0'
     }
   }
 });
